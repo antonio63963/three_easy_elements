9e80350bd9be50af31b4ec6fce896b2e86c853d5
a.fomin <a.fomin@anc.net.ua>
2025-11-17 16:51:39 +0200

    test wait in child

--- create_process/hw/print_wait.c
+++ create_process/hw/print_wait.c
@@ -3,18 +3,24 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-  
+int main(int argc, char *argv[])
+{
   int rc = fork();
-  if(rc< 0) {
+  if (rc < 0)
+  {
     perror("OOOPs!!! Failed fork...");
-  }else if(rc == 0) {
-    printf("Hello\n");
-  }else {
-    wait(NULL);
-    printf("Goodbye\n");
   }
-  
+  else if (rc == 0)
+  {
+    printf("Hello, (pid: %d)\n", getpid());
+    pid_t chP = wait(NULL);
+    printf("CHP: %d\n", chP);
+  }
+  else
+  {
+    pid_t wPid = wait(NULL);
+    printf("Goodbye (wPid: %d), (pid: %d)\n", wPid, getpid());
+  }
 
   return 0;
 }
